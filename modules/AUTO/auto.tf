5ba8434df4aa090e20bd77b3e6d4157089f636c0
saikishorekaay <saikishore.rajendran@kaaylabs.com>
2021-06-12 03:34:35 +0530

    AG added

--- modules/AUTO/auto.tf
+++ modules/AUTO/auto.tf
@@ -4,7 +4,7 @@ resource "aws_launch_configuration" "terraform" {
   instance_type     = "t2.micro"
   iam_instance_profile = "ecsinstancerole"
   key_name          = "sai"
-  user_data         = "#!/bin/bash\necho ECS_CLUSTER= ""${aws_ecs_cluster.terraform.id}"" >> /etc/ecs/ecs.config;\necho ECS_BACKEND_HOST= >> /etc/ecs/ecs.config;"
+  user_data         = "#!/bin/bash\necho ECS_CLUSTER= terraform >> /etc/ecs/ecs.config;\necho ECS_BACKEND_HOST= >> /etc/ecs/ecs.config;"
   security_groups   = ["${aws_security_group.terraform.id}"]
   associate_public_ip_address  = "true"
   enable_monitoring = "enable"
@@ -18,7 +18,7 @@ resource "aws_launch_configuration" "terraform" {
 
 resource "aws_autoscaling_group" "terraform" {
   name                 = "terraform"
-  vpc_zone_identifier  = ["${aws_subnet.terraform.*.]id[0]}","${aws_subnet.terraform.*.]id[1]}"]
+  vpc_zone_identifier  = ["${aws_subnet.terraform.*.id[0]}","${aws_subnet.terraform.*.id[1]}"]
   launch_configuration = aws_launch_configuration.terraform.name
   desired_capacity     = 1
   min_size             = 1
